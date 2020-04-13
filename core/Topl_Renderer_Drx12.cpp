2beba6df37138d3bfaee99a71e4ce5a5a65324c9
Anton O <ao.omega2000@gmail.com>
2020-04-13 14:01:14 -0500

    Initial Commit

---
+++ core/Topl_Renderer_Drx12.cpp
@@ -0,0 +1,127 @@
+#include "Topl_Renderer_Drx12.hpp"
+
+DRX12_INIT_STATUS Topl_Renderer_Drx12::init(void){
+    HRESULT hr;
+
+	IDXGIFactory4* dxgiFactory;
+	hr = CreateDXGIFactory1(IID_PPV_ARGS(&dxgiFactory));
+	if (FAILED(hr)) return DRX12_Init_Fail_CreateFactory;
+
+	IDXGIAdapter1* drxAdapter;
+	int drxAdapterIdx = 0;
+	bool drxAdapterFound = false;
+
+	while (dxgiFactory->EnumAdapters1(drxAdapterIdx, &drxAdapter) != DXGI_ERROR_NOT_FOUND) {
+		DXGI_ADAPTER_DESC1 drxAdapterDesc;
+		drxAdapter->GetDesc1(&drxAdapterDesc);
+		
+		if (drxAdapterDesc.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) {
+			drxAdapterIdx++;
+			continue; // Adapter not found
+		} else {
+			hr = D3D12CreateDevice(drxAdapter, D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr);
+			// DOES NOT CREATE DEVICE, JUST TESTS FOR D3D12 SUPPORT
+			if (SUCCEEDED(hr)) {
+				drxAdapterFound = true;
+				break; // Adapter found!!!
+			}
+			else {
+				drxAdapterIdx++;
+				continue; // Adapter does not support DirectX 12
+			}
+			// OPTIMIZE THIS CODE!!!!!!!!
+		}
+	}
+
+	if (!drxAdapterFound) return DRX12_Init_Fail_NoAdapter;
+
+	hr = D3D12CreateDevice(drxAdapter, D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&m_device));
+	if (FAILED(hr)) return DRX12_Init_Fail_CreateDevice;
+
+	// Command queue time!
+	// D3D12_COMMAND_QUEUE_DESC drxCmdQueue;
+
+	D3D12_COMMAND_QUEUE_DESC cmdQueueDesc = { }; // Using all default values
+	hr = m_device->CreateCommandQueue(&cmdQueueDesc, IID_PPV_ARGS(&m_cmdQueue));
+	if (FAILED(hr)) return DRX12_Init_Fail_CreateCmdQueue;
+
+
+	// Swap Chain Time!
+
+	DXGI_SWAP_CHAIN_DESC drxSwapChainDesc;
+
+	DXGI_MODE_DESC backBufferDesc = { };
+	backBufferDesc.Width = 0; // This scales it to default
+	backBufferDesc.Height = 0; // This scales it to default
+	backBufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+
+	DXGI_SAMPLE_DESC sampleDesc = { };
+	sampleDesc.Count = 1; // No multi-sampling
+
+	DXGI_SWAP_CHAIN_DESC swapChainDesc = { };
+	// swapChainDesc.BufferCount = 3; // 3 back buffers
+	swapChainDesc.BufferCount = m_frameBuffCount;
+	swapChainDesc.BufferDesc = backBufferDesc;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // render to this swapchain
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD; // dxgi will discard buffer after call
+	swapChainDesc.OutputWindow = hwnd; // passed as arg
+	swapChainDesc.SampleDesc = sampleDesc;
+	swapChainDesc.Windowed = true;
+
+	IDXGISwapChain* tmpSwapChain;
+	dxgiFactory->CreateSwapChain(m_cmdQueue, &swapChainDesc, &tmpSwapChain);
+	m_swapChian = static_cast<IDXGISwapChain3*>(tmpSwapChain); // Cast the temp structure
+	m_frameIndex = m_swapChian->GetCurrentBackBufferIndex();
+
+	
+	// Descriptor Heap Time!
+
+	D3D12_DESCRIPTOR_HEAP_DESC rtvHeapDesc = { };
+	rtvHeapDesc.NumDescriptors = m_frameBuffCount;
+	rtvHeapDesc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+	rtvHeapDesc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE; // Not shader visible
+
+	hr = m_device->CreateDescriptorHeap(&rtvHeapDesc, IID_PPV_ARGS(&m_rtvDescriptHeap));
+	if (FAILED(hr)) return DRX12_Init_Fail_CreateDescHeap;
+
+	m_rtvDescriptorSize = m_device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+	CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(m_rtvDescriptHeap->GetCPUDescriptorHandleForHeapStart());
+
+	for (short b = 0; b < m_frameBuffCount; b++) {
+		hr = m_swapChian->GetBuffer(b, IID_PPV_ARGS(&m_renderTargets[b]));
+		if (FAILED(hr)) return DRX12_Init_Fail_SwpChainGetBuffer;
+
+		m_device->CreateRenderTargetView(m_renderTargets[b], nullptr, rtvHandle);
+		rtvHandle.Offset(1, m_rtvDescriptorSize);
+	}
+
+	// Command Allocator Time!
+	// Each rtv needs its own command allocator
+
+	for (short c = 0; c < m_frameBuffCount; c++) {
+		hr = m_device->CreateCommandAllocator(D3D12_COMMAND_LIST_TYPE_DIRECT, IID_PPV_ARGS(&m_cmdAllocator[c]));
+		if (FAILED(hr)) return DRX12_Init_Fail_CreateCmdAllocator;
+	}
+
+	// Command List Time!
+
+	hr = m_device->CreateCommandList( 0, D3D12_COMMAND_LIST_TYPE_DIRECT, m_cmdAllocator[0], NULL, IID_PPV_ARGS(&m_cmdGList));
+	if (FAILED(hr)) return DRX12_Init_Fail_CreateCmdList;
+
+	m_cmdGList->Close();
+
+	// Fence Time!
+
+	for (short f = 0; f < m_frameBuffCount; f++) {
+		hr = m_device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&m_fence[f]));
+		if (FAILED(hr)) return DRX12_Init_Fail_CreateFence;
+
+		m_fence[f] = 0; // Starting value of fence
+	}
+
+	m_fenceEvent = CreateEvent(nullptr, FALSE, FALSE, nullptr);
+	if (m_fenceEvent == nullptr) return DRX12_Init_Fail_CreateFenceEvent;
+
+    // Everything went fine...
+	return DRX12_Init_Good;
+}
---
+++ core/Topl_Renderer_Drx12.hpp
@@ -0,0 +1,52 @@
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+
+// Windows runtime library COM-Specific items
+#include <wrl.h>
+using namespace Microsoft::WRL;
+
+#include "d3dx12.h" // Helper that also includes d3d12.h
+#include <dxgi1_4.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+typedef enum {
+    DRX12_Init_Good = 1,
+    DRX12_Init_Fail = -1,
+    DRX12_Init_Fail_CreateFactory = -2,
+    DRX12_Init_Fail_NoAdapter = -3,
+    DRX12_Init_Fail_CreateDevice = -4,
+    DRX12_Init_Fail_CreateCmdQueue = -5,
+    DRX12_Init_Fail_CreateDescHeap = -6,
+    DRX12_Init_Fail_SwpChainGetBuffer = -7,
+    DRX12_Init_Fail_CreateCmdAllocator = -8,
+    DRX12_Init_Fail_CreateCmdList = -9,
+    DRX12_Init_Fail_CreateFence = -10,
+    DRX12_Init_Fail_CreateFenceEvent = -11
+} DRX12_INIT_STATUS;
+
+class Topl_Drx12 {
+private:
+    const int m_frameBuffCount = 3; // For triple buffering
+	ID3D12Device* m_device;
+	IDXGISwapChain3* m_swapChian; // For switching between render targets
+	ID3D12CommandQueue* m_cmdQueue; // contatiner of command list
+	ID3D12DescriptorHeap* m_rtvDescriptHeap; // descriptor heap to hold resources
+	ID3D12Resource* m_renderTargets[frameBuffCount]; // Render targets
+	ID3D12CommandAllocator* m_cmdAllocator[frameBuffCount]; // Command allocator for each frame
+	ID3D12GraphicsCommandList* m_cmdGList; // Command list to record and render onto frame
+	
+	ID3D12Fence* m_fence[frameBuffCount]; // For each allocator
+	HANDLE m_fenceEvent; // Event when handle is unlocked by GPU
+	UINT64 m_fenceValue[frameBuffCount]; // Incremented each frame
+	
+	int m_frameIndex; // current rtv we are on
+	int m_rtvDescriptorSize; // Size of rtv descriptor on the device (front/back buffers will be same size)
+
+    DRX12_INIT_STATUS initStatus;
+public:
+    Topl_Drx12(void){ initStatus = init(); }
+
+    DRX12_INIT_STATUS init(void);
+    void print(void);
+};
