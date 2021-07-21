aa59d1849b27c3df3bd1bb504bed6ad01c6b16a1
Anton O <ao.omega2000@gmail.com>
2021-07-21 16:14:38 -0500

    Scrap File3D Parsing

--- geo/primitives/Geo_Model3D.hpp
+++ geo/primitives/Geo_Model3D.hpp
@@ -1,23 +1,23 @@
 #include "Geometry.hpp"
 
-// #include "FileIO.hpp"
-#include "File3D.hpp"
+#include "FileIO.hpp"
+// #include "File3D.hpp"
 
 class Geo_Model3D : public Geo_RenderObj {
 public:
     Geo_Model3D(){  } // for testing
     // Model Constructor
-    Geo_Model3D(const File3D_Node* node)
+    /* Geo_Model3D(const File3D_Node* node)
     : Geo_RenderObj(node->getVertexCount(), node->getIndexCount()){
 		mNode = node;
 		
 		fillRenderObject();
-    }
+    } */
 
 	void log(const char* fileName); // prints contents for debugging
 private:
-	friend class File3D_Node;
-	const File3D_Node* mNode;
+	// friend class File3D_Node;
+	// const File3D_Node* mNode;
 
     void genVertices(Eigen::Vector3f* data) override;
 	void genNormals(Eigen::Vector3f* data) override;
